910e75b8e6f7431c69092667b896713e92757194
Holden Thomas <holden.thomas@gmail.com>
2012-12-05 09:53:57 -0600

    lots

---
+++ php/contact-config.php
@@ -0,0 +1,17 @@
+<?php
+/** 
+ * The base configurations of the contact form.
+ *
+ */
+ 
+/* Your email address. The contact form will send emails to this address. You can add multiple addresses separated by commas. */
+$my_email_address='your@email.com';
+
+/* You may set Cc and Bcc values for the form here. Leave them blank if you don't wish to send any Cc/Bcc. */
+$cc='';
+$bcc='';
+
+/* Set this to PHP or HTML depending on the contact page you are using. */
+$contact_form_version='HTML';
+
+?>
---
+++ php/newsletter-config.php
@@ -0,0 +1,15 @@
+<?php
+/* Newsletter Signup Settings File */
+/* Theme Name: Ego HTML */
+/* Author: UDTHEMES (http://udthemes.com) */
+
+// Your Newsletter Service Provider: "CampaignMonitor" or "MailChimp"
+$newsletter_service_provider='MailChimp';
+
+// Your API Key
+$api_key='';
+
+// Your List ID
+$list_id='';
+
+?>
